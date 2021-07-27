b693162f466cf1536ff49ba5b6dcb89d43927f41
ryanlimjr <ryanlimjr@yahoo.com.sg>
2021-07-28 00:23:51 +0800

    Add documentation and type hints

--- src/Commands/GenerateQuote.py
+++ src/Commands/GenerateQuote.py
@@ -1,19 +1,52 @@
+from json.decoder import JSONDecodeError
+
 import requests as req
+from constants import ERROR_GEN_QUOTE, ERROR_PARSE_RESPONSE
+from requests import Response
+from requests.exceptions import RequestException
+from telegram import Update
+from telegram.ext import CallbackContext
+
 
 class GenerateQuoteCommand:
-    ERROR = 'There is problems generating a quote please try again later'
+    # ERROR_GEN_QUOTE = "There was an error generating the quote."
+    # ERROR_PARSE_RESPONSE = "There was an error parsing the response."
 
     @staticmethod
-    def parseResponse(response):
-        quote = response.json()[0]
-        return quote['q'] + "\n\n - " + quote['a']
+    def parseResponse(response: Response) -> str :
+        """
+        Parses the response from the quotes API and reformats it. 
+
+        Args:
+            response (Response): response object from doing a get request to the quotes api.
+
+        Returns:
+            String: the reformated quote.
+        """
+        try:
+            quote = response.json()[0]
+            return quote['q'] + "\n\n - " + quote['a']
+        except JSONDecodeError as e:
+            return ERROR_PARSE_RESPONSE
 
     @staticmethod
-    def getQuote():
-        response = req.get('https://zenquotes.io/api/random') 
-        statusCode = response.status_code
-        return GenerateQuoteCommand.parseResponse(response) if statusCode == 200 else GenerateQuoteCommand.ERROR
+    def getQuote() -> str :
+        """
+        Does a get request to the quote API and extracts the quote
+
+        Returns:
+            String: quote that was requested from the quotes API 
+        """
+        try :
+            response = req.get('https://zenquotes.io/api/random') 
+            statusCode = response.status_code
+            return GenerateQuoteCommand.parseResponse(response) if statusCode == 200 else ERROR_GEN_QUOTE
+        except RequestException as e:
+            return ERROR_GEN_QUOTE
 
     @staticmethod
-    def execute(update, context):
+    def execute(update: Update, context: CallbackContext) -> None:
+        """
+        Sends response back to telegram user
+        """
         update.message.reply_text(GenerateQuoteCommand.getQuote())
