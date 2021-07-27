20d83e89e77755b2b1b1ff6a3a702457898d26ac
ryanlimjr <ryanlimjr@yahoo.com.sg>
2021-07-28 00:28:58 +0800

    Add necessary authentication functions to access Google Sheets api

--- src/Commands/DispBoatAlloc.py
+++ src/Commands/DispBoatAlloc.py
@@ -1,6 +1,29 @@
+import json
+import os
 
+import gspread
+import pandas as pd
+from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+
+"""uncomment this when testing locally"""
+load_dotenv()
+GOOGLE_CREDS=json.loads(os.environ.get("GOOGLE_CREDS"))
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_dict(GOOGLE_CREDS, scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
 class DispBoatAllocCommand:
 
     @staticmethod
-    def getBoatAlloc():
-        return 0 
+    def test():
+        sheet = client.open("telegram bot testing")
+        # Extract and print all of the values
+        worksheet = sheet.get_worksheet(1)
+        
+
+DispBoatAllocCommand.test()
