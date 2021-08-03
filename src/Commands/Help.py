4cee09b23f1835b5cd84336e9a5ce4ee4a1f5c6b
ryanlimjr <ryanlimjr@yahoo.com.sg>
2021-08-03 11:34:13 +0800

    fix bug in help.py

--- src/Commands/Help.py
+++ src/Commands/Help.py
@@ -1,10 +1,6 @@
 from constants import HELP_MESSAGE
 class HelpCommand:
-
-    @staticmethod
-    def generateHelpMessage():
-        return 'help'
-
+    
     @staticmethod
     def execute(update , context):
-        update.message.reply_text(HelpCommand.generateHelpMessage())
+        update.message.reply_text(HELP_MESSAGE)
